bf1089453a2be703b082e81cd82cb7a602642661
Inamura JIN <magic.inamura.jin@gmail.com>
2024-06-27 13:58:50 +0900

    Bake to texture name
    どのテクスチャにベイクするのかを可視化しました

--- ui.py
+++ ui.py
@@ -10,10 +10,15 @@ class SimpleBakePanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         scene = context.scene
+        obj = context.active_object
 
         # Simple Bakeボタン
         row = layout.row()
         row.operator("object.simple_bake_operator", text="Simple Bake")
+
+        # メッセージボックス
+        row = layout.row()
+        row.label(text=self.get_bake_message(context))
 
         # Auto Save チェックボックス
         row = layout.row()
@@ -22,6 +27,29 @@ class SimpleBakePanel(bpy.types.Panel):
         # Bake Type ラジオボタン
         layout.label(text="Bake Type")
         layout.prop(scene, "simple_bake_type", expand=True)
+
+    def get_bake_message(self, context):
+        obj = context.active_object
+        if obj and obj.type == 'MESH':
+            if obj.material_slots:
+                for mat_slot in obj.material_slots:
+                    if mat_slot.material and mat_slot.material.use_nodes:
+                        active_node = self.get_active_image_node(context, mat_slot.material.node_tree)
+                        if active_node and active_node.image:
+                            return f"Bake to ***{active_node.image.name.upper()}***"
+                return "No Bakeable Material!"
+            else:
+                return "No Bakeable Material!"
+        else:
+            return "No Bakeable Material!"
+
+    def get_active_image_node(self, context, node_tree):
+        for area in context.screen.areas:
+            if area.type == 'NODE_EDITOR':
+                for space in area.spaces:
+                    if space.type == 'NODE_EDITOR' and space.node_tree == node_tree:
+                        return space.node_tree.nodes.active
+        return None
 
 
 class BakeSettingsPanel(bpy.types.Panel):
@@ -36,17 +64,17 @@ class BakeSettingsPanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         scene = context.scene
-        
+
         # Bake Settingsの項目
         col = layout.column(align=True)
         col.prop(scene, "simple_bake_uv_channel", text="Set Bake UV")
         col.prop(scene, "simple_bake_return_to_original_uv", text="Return to Selected UV")
-        
+
         # Samples ラジオボタン
         col.label(text="Samples")
         row = col.row(align=True)
         row.prop(scene, "simple_bake_samples", expand=True)
-        
+
         # Return to Render Samples チェックボックス
         col.prop(scene, "simple_bake_return_to_render_samples", text="Return to Render Samples")
 
